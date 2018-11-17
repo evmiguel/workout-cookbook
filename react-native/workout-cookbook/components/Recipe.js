4d69ff99546f0f663c097165c33c7a28328828ac
Erika Miguel <ms.erikamiguel@gmail.com>
2018-11-17 16:38:35 -0500

    #11 adding skeleton content for workout recipe

--- react-native/workout-cookbook/components/Recipe.js
+++ react-native/workout-cookbook/components/Recipe.js
@@ -7,16 +7,48 @@ class Recipe extends Component {
 	    const { name } = navigation.state.params
 
 	    return {
-	      title: (name === name.toLowerCase())
-	      		? name.split(' ').map(w => w[0].toUpperCase() + name.substr(1).toLowerCase()).join(' ')
-	      		: name
+	      title: titleCase(name)
 	    }
   	}
 
 	render() {
+		const { name, total_time, instructions } = this.props.recipe
 		return (
-			<View></View>
+			<View style={styles.container}>
+				<Text>Recipe for:</Text>
+				<Text>{titleCase(name)}</Text>
+				<Text>Total Time: {total_time} {total_time > 1 ? 'minutes' : 'minutes'}</Text>
+				<Text>Instructions:</Text>
+				{
+					instructions.map(i => (
+						<View key={i.step} style={styles.instruction}>
+							<Text style={{marginRight: 10}}>{i.step}</Text>
+							<Text>{i.time}</Text>
+						</View>
+					))
+				}
+			</View>
 		)
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	instruction: {
+		flexDirection: 'row'
+	}
+})
+
+function titleCase(str){
+	if (typeof str !== 'undefined') {
+		let string = (str === str.toLowerCase())
+      		? str.split(' ').map(w => w[0].toUpperCase() + str.substr(1).toLowerCase()).join(' ')
+      		: str
+		return string
 	}
 }
 
