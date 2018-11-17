bbfe86b8118ed7d8e7c776b82d3cc26a1ebaf4a1
Erika Miguel <ms.erikamiguel@gmail.com>
2018-11-17 16:52:33 -0500

    #11 simple styling for a workout recipe

--- react-native/workout-cookbook/components/Recipe.js
+++ react-native/workout-cookbook/components/Recipe.js
@@ -15,15 +15,15 @@ class Recipe extends Component {
 		const { name, total_time, instructions } = this.props.recipe
 		return (
 			<View style={styles.container}>
-				<Text>Recipe for:</Text>
-				<Text>{titleCase(name)}</Text>
-				<Text>Total Time: {total_time} {total_time > 1 ? 'minutes' : 'minutes'}</Text>
-				<Text>Instructions:</Text>
+				<Text style={styles.title}>Recipe For</Text>
+				<Text style={[styles.recipeName, {marginBottom: 15}]}>{titleCase(name)}</Text>
+				<Text style={{fontSize: 20, marginBottom: 40}}>Total Time: {total_time} {total_time > 1 ? 'minutes' : 'minutes'}</Text>
+				<Text style={{fontSize: 30, marginBottom: 15}}>Instructions:</Text>
 				{
 					instructions.map(i => (
-						<View key={i.step} style={styles.instruction}>
-							<Text style={{marginRight: 10}}>{i.step}</Text>
-							<Text>{i.time}</Text>
+						<View key={i.step} style={[styles.instruction, {marginBottom: 5}]}>
+							<Text style={{fontSize: 25, marginRight: 5}}>{i.step} for</Text>
+							<Text style={{fontSize: 25}}>{i.time} {i.time > 1 ? 'minutes' : 'minute' }</Text>
 						</View>
 					))
 				}
@@ -38,8 +38,17 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center'
 	},
+	title: {
+		fontSize: 30,
+		fontWeight: 'bold'
+	},
 	instruction: {
 		flexDirection: 'row'
+	},
+	recipeName: {
+		fontSize: 50,
+		fontWeight: 'bold',
+		color: 'red'
 	}
 })
 
